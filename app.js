b98e7d1d345e5964699105c69e3f839c80f950e1
Michelle Castellanos <m.castellanos12@gmail.com>
2017-01-26 13:27:17 -0600

    initial commit

---
+++ app.js
@@ -0,0 +1,48 @@
+// dependency for inquirer npm package
+var inquirer = require("inquirer");
+
+var questions = require('./basic.json')
+
+// constructor function used to create flashcard objects
+function Flashcard(front, back) {
+  this.front = front;
+  this.back = back;
+}
+
+// creates the printInfo method and applies it to all programmer objects
+Flashcard.prototype.printInfo = function() {
+  console.log("Question: " + this.front + "\nAnswer: " + this.back );
+};
+
+// variable we will use to count how many times our questions have been asked
+var count = 0;
+
+var questions = require('./basic.json');
+
+var askQuestion = function() {
+  // if statement to ensure that our questions are only asked five times
+  if (count < 5) {
+    // runs inquirer and asks the user a series of questions whose replies are
+    // stored within the variable answers inside of the .then statement
+    inquirer.prompt([
+      {
+        name: "name",
+        message: questions[count].front
+      }
+    ]).then(function(answers) {
+
+      // add one to count to increment our recursive loop by one
+      count++;
+      // run the askquestion function again so as to either end the loop or ask the questions again
+      askQuestion();
+    });
+    // else statement which prints "all questions asked" to the console
+    // when the code has been run five times
+  }
+  else {
+    console.log("All questions asked");
+  }
+};
+
+// call askquestion to run our code
+askQuestion();
